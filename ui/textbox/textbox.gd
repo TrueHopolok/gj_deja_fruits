7214a9eab4e5939ca2e124a207115ca5e13f8bbc
Richard Shashalevich <richardsasalevich@outlook.com>
2026-01-09 18:50:19 +0200

    text box done

---
+++ ui/textbox/textbox.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var _current_text : String = ""
+var _paragraphs: Array
+
+@onready var _label : Label = $Label
+
+var _char_index : int = 0
+var _paragraph_index: int = 0
+func _process(delta: float) -> void:
+	if !visible:
+		return
+	if _char_index<len(_current_text):
+		_label.text += _current_text[_char_index]
+		_char_index+=1
+	if Input.is_action_just_pressed("left_click"):
+		if _char_index<len(_current_text):
+			_label.text = _current_text
+			_char_index = len(_current_text)
+		elif _paragraph_index < len(_paragraphs):
+			_label.text = ""
+			_char_index = 0
+			_current_text = _paragraphs[_paragraph_index]
+			_paragraph_index+=1
+		else:
+			visible=false
+func set_text(paragraphs: Array) -> void:
+	_paragraph_index = 1
+	_paragraphs = paragraphs
+	_current_text = paragraphs[0]
+	_char_index = 0
