bbd09b9db1036e468a101b888abb3d01471d82eb
Richard Shashalevich <richardsasalevich@outlook.com>
2026-01-10 15:05:02 +0200

    Small changes

--- ui/textbox/textbox.gd
+++ ui/textbox/textbox.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
+@onready var _label : Label = $Label
+var _char_index : int = 0
 var _current_text : String = ""
+var _paragraph_index: int = 0
 var _paragraphs: Array
 
-@onready var _label : Label = $Label
 
-var _char_index : int = 0
-var _paragraph_index: int = 0
 func _process(_delta: float) -> void:
 	if !visible:
 		return
+		
 	if _char_index<len(_current_text):
 		_label.text += _current_text[_char_index]
 		_char_index+=1
+		
 	if Input.is_action_just_pressed("skip_dialog"):
 		if _char_index<len(_current_text):
 			_label.text = _current_text
@@ -24,6 +26,8 @@ func _process(_delta: float) -> void:
 			_paragraph_index+=1
 		else:
 			visible=false
+			
+			
 func set_text(paragraphs: Array) -> void:
 	_paragraph_index = 1
 	_paragraphs = paragraphs
