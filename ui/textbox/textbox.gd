9bb3aad70077efb8159f051703c7ff70fec5b2ee
TrueHopolok <vadim.truhanov@inbox.lv>
2026-01-11 14:49:59 +0200

    ADD: ded into dialogues

--- ui/textbox/textbox.gd
+++ ui/textbox/textbox.gd
@@ -4,15 +4,22 @@ extends Node2D
 
 signal finished_outro
 
+@export var text_speed: float = 0.1
+@onready var _dedsprite: Node2D = $DedSprite
+@onready var _dedmounth: AnimatedSprite2D = $DedSprite/DedSprite/Mounth
+@onready var _dedeyebrows: AnimatedSprite2D = $DedSprite/DedSprite/Eyebrows
 @onready var _label : Label = $Label
 @onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _aniplayer: AnimationPlayer = $AnimationPlayer
+var _emotions: Dictionary = {}
 var _char_index : int = 0
 var _current_text : String = ""
 var _paragraph_index: int = 0
 var _paragraphs: Array[String]
 var active: bool = false
 var printing: bool = false
+var _time: float
+var _executing: bool
 
 
 func _ready() -> void:
@@ -22,41 +29,86 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if !printing: return
 
-	if _char_index < len(_current_text):
-		_label.text += _current_text[_char_index]
-		_char_index+=1
-		
+	if _executing:
+		if _time < 0:
+			if _char_index < len(_current_text):
+				_label.text += _current_text[_char_index]
+				_char_index += 1
+			else: _stop_talking()
+		else: _time -= _delta
+
 	if Input.is_action_just_pressed(&"skip_dialog"):
 		if _char_index < len(_current_text):
 			_label.text = _current_text
 			_char_index = len(_current_text)
-		elif _paragraph_index < len(_paragraphs):
+		elif _paragraph_index < len(_paragraphs) - 1:
+			_paragraph_index += 1
+			_start_talking()
 			_label.text = ""
 			_char_index = 0
 			_current_text = _paragraphs[_paragraph_index]
-			_paragraph_index += 1
 		else:
 			printing = false
 			_play_outro()
 
 
-func set_text(paragraphs: Array[String], _is_boy: Dictionary) -> void:
+func set_text(paragraphs: Array[String], emotions: Dictionary) -> void:
+	if len(paragraphs) == 0: 
+		finished_outro.emit()
+		return
 	active = true
-	_paragraph_index = 1
+	_paragraph_index = 0
 	_paragraphs = paragraphs
 	_current_text = paragraphs[0]
 	_char_index = 0
 	_label.text = ""
+	_emotions = emotions
 	_play_intro()
 
 
+func _hide_sprites() -> void:
+	_dedsprite.modulate = Color(0, 0, 0)
+	# TODO: boy black modulate
+
+
+func _stop_talking() -> void:
+	_executing = false
+	_dedmounth.play("idle")
+	# TODO: boy idle animtion
+
+
+func _start_talking() -> void:
+	_executing = true
+	_time = text_speed
+	if _emotions.has(_paragraph_index):
+		match _emotions[_paragraph_index]:
+			'boy':
+				# TODO: boy white modulate
+				# TODO: boy talk animation
+				_dedsprite.modulate = Color(0, 0, 0)
+			_:
+				_dedsprite.modulate = Color(1, 1, 1)
+				_dedmounth.play("talk")
+				_dedeyebrows.play(_emotions[_paragraph_index])
+				# TODO: boy black modulate
+	else:
+		_dedsprite.modulate = Color(1, 1, 1)
+		_dedmounth.play("talk")
+		_dedeyebrows.play("normal")
+		# TODO: boy black modulate
+
+
 func _play_intro() -> void:
+	_hide_sprites()
+	_stop_talking()
 	visible = true
 	_sprite.play(&"intro")
 	_aniplayer.play(&"intro")
 
 
 func _play_outro() -> void:
+	_hide_sprites()
+	_stop_talking()
 	_aniplayer.play(&"outro")
 
 
@@ -64,6 +116,7 @@ func _finished_animation(_anim_name: StringName) -> void:
 	if _anim_name == &"intro":
 		printing = true
 		_sprite.play(&"idle")
+		_start_talking()
 	elif _anim_name == &"outro":
 		visible = false
 		active = false
