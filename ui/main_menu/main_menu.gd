aee1baf5e2ef3b8a5c626614d97a2f2675a2b69f
Richard Shashalevich <richardsasalevich@outlook.com>
2026-01-11 17:06:46 +0200

    ADD: menu, loading screen and cutscene

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -7,3 +7,7 @@ func _on_level_select_pressed() -> void:
 
 func _on_audio_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/audio_settings/audio_settings.tscn")
+
+
+func _on_new_game_pressed() -> void:
+	Transition.change_scene(load("res://ui/intro/intro.tscn"))
