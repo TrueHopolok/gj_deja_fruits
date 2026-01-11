3a4b79a429db15a2cd0e421b2130b5c2e3a88a44
TrueHopolok <vadim.truhanov@inbox.lv>
2026-01-11 20:13:05 +0200

    FIX: player restart

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -19,10 +19,9 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"restart"): Transition.change_scene(load(get_tree().current_scene.name))
-
 	if _moving: return
 	if block.values().any(func(bl: bool)->bool: return bl): return
+	if event.is_action_pressed(&"restart"): Transition.change_scene(load(get_tree().current_scene.name))
 
 	var pos: Vector2i = _field.local_to_map(position)
 	var dir: Vector2i = Vector2i.ZERO
