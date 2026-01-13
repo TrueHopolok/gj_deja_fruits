be94e4158ce17cf19cb58cdc304c8230f10bf0f7
TrueHopolok <vadim.truhanov@inbox.lv>
2026-01-13 19:23:10 +0200

    FIX: fixed raised issues in #17 conserning audio settings

--- ui/audio_settings/audio_settings.gd
+++ ui/audio_settings/audio_settings.gd
@@ -1,5 +1,21 @@
-extends Panel
+class_name AudioSettings
+extends Node
 
 
-func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
+const AUDIO_CFG_PATH = "user://audio.cfg"
+
+var config: ConfigFile = ConfigFile.new()
+
+
+func _ready() -> void:
+	read()
+
+
+func read() -> void:
+	var err := config.load(AUDIO_CFG_PATH)
+	if err != Error.OK: print(err)
+
+
+func save() -> void:
+	var err := config.save(AUDIO_CFG_PATH)
+	if err != Error.OK: print(err)
